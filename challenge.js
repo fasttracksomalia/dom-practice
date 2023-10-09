17a07cef1b38641696fe5cc9a711498f35e01fe6
= <=>
2023-10-09 20:14:11 +0100

    function do..while

--- challenge.js
+++ challenge.js
@@ -1,43 +1,61 @@
 
 // write a for loop that prints your name 20 times
 
-for(let i = 0; i < 20; i++) {
-    console.log("Ahmed")
-}
+// for(let i = 0; i < 20; i++) {
+//     console.log("Ahmed")
+// }
 
 
 // write a while that prints your surname 20 times
+// let i = 0;
+// while (i < 20) {
+//     console.log("Dini");
+//     i++;
+// }
+
+// convert the while loop above to a do...while loop whose body runs once at least
+
 let i = 0;
-while (i < 20) {
-    console.log("Dini");
-    i++;
-}
+
+// do {
+//     console.log("kdhabjksdfbdjkaabdh")
+//     i++;
+// } while (i < 5);
 
 
 // write if/else if, chained code to check what day of the day it is and print something
 
-let today = "monday";
+// let today = "monday";
 
-if(today == "Monday") {
-    console.log("It is Coding Day")
-} else if (today == "Tuesday") {
-    console.log("It is my off day")
-} else {
-    console.log("ask me personally which day")
-}
+// if(today == "Monday") {
+//     console.log("It is Coding Day")
+// } else if (today == "Tuesday") {
+//     console.log("It is my off day")
+// } else {
+//     console.log("ask me personally which day")
+// }
 
 // convert the above if/else to switch
 
-switch (today) {
-    case "Monday":
-        console.log("It is Coding Day, from switch")
-        break;
-    case "Tuesday":
-        console.log("It is my off day, from switch")
-        break;
+// switch (today) {
+//     case "Monday":
+//         console.log("It is Coding Day, from switch")
+//         break;
+//     case "Tuesday":
+//         console.log("It is my off day, from switch")
+//         break;
 
-    default:
-        console.log("ask me personally which day, from switch")
-        break;
+//     default:
+//         console.log("ask me personally which day, from switch")
+//         break;
+// }
+
+
+
+// write a function that take two number and multiplies them and returns the result then prints it
+
+
+function multiply (num1,num2){
+    return num1 * num2
 }
-
+console.log (multiply(4,5))
